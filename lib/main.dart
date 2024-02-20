82dfe577da4a013beb5e8a808cedaa49648c2516
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2024-02-20 21:18:23 +0200

    Update text for testing

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Text Parsing in Flutter',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Demo Page'),
     );
   }
 }
@@ -39,26 +39,29 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CustomRichText(
-              richTextData: RegexRichText().textTransformation(
-                context: context,
-                text:
-                    'Some text <b>BOLD</b>, <i>italic</i> and <b><i>bold and italic</i></b> and <b><i><u> bold and italic and underline</b></i></u>  and <i><u><b> italic and underline and bold</i></u></b>',
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CustomRichText(
+                richTextData: RegexRichText().textTransformation(
+                  context: context,
+                  text:
+                      'I wanna tell you something <b>IMPORTANT</b> and <i>delicate</i> but <b><i><u> make it special</u></i></b>! Let\'s chat more…',
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            CustomRichText(
-              richTextData: RegexRichText().textTransformation(
-                context: context,
-                text:
-                    'Some text <b>BOLD</b>, <i>italic</i> and <b><i>bold and italic</i></b> and <b><i><u> bold and italic and underline</b></i></u>  and <i><u><b> italic and underline and bold</i></u></b>',
+              const SizedBox(height: 20),
+              CustomRichText(
+                richTextData: RegexRichText().textTransformation(
+                  context: context,
+                  text:
+                      'Some text <b>BOLD</b>, <i>italic</i> and <b><i>bold and italic</i></b> and <b><i><u> bold and italic and underline</b></i></u>  and <i><u><b> italic and underline and bold</i></u></b>',
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
